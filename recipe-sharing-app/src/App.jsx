a5f697a7a1b4ae53fdb573f4292d1f13d3c7c651
shaniqhope <shaniqhope@gmail.com>
2024-11-24 21:23:02 +0000

    Implementing Advanced Search and Filtering Capabilities

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -2,17 +2,17 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import RecipeList from './components/RecipeList';
 import AddRecipeForm from './components/AddRecipeForm';
-import SearchBar from './components/SearchBar';
+import RecipeDetails from './components/RecipeDetails';
 
 const App = () => {
   return (
     <Router>
       <div style={{ textAlign: 'center', fontFamily: 'Arial, sans-serif' }}>
         <h1>Recipe Sharing Application</h1>
-        <SearchBar />
         <AddRecipeForm />
         <Routes>
           <Route path="/" element={<RecipeList />} />
+          <Route path="/recipe/:id" element={<RecipeDetails />} />
         </Routes>
       </div>
     </Router>
@@ -20,5 +20,6 @@ const App = () => {
 };
 
 export default App;
+
 
 
