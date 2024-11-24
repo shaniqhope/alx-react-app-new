362e60d51ee1700b2be0e3d40d09b35125826980
shaniqhope <shaniqhope@gmail.com>
2024-11-24 19:45:13 +0000

    Added Counter component with state and hooks

---
+++ alx-react-app-new/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Counter from './components/Counter';
+
+const App = () => {
+  return (
+    <div>
+      <h1 style={{ textAlign: 'center' }}>Simple Counter Application</h1>
+      <Counter />
+    </div>
+  );
+};
+
+export default App;
